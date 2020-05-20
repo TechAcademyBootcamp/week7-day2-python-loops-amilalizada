01c379ea43dbd0586d86b0b33bc1e19b17afa824
amilalizada <elizade_amil@inbox.ru>
2020-05-20 21:05:19 +0400

    upgrade

--- 05-Number-Puzzle/puzzle.py
+++ 05-Number-Puzzle/puzzle.py
@@ -1,5 +1,13 @@
 print('Program haqqinda melumat...')
 array=[]
 number = input('eded daxil edin: ')
-while not number.digit() or int(number)<0:
+while not number.isdigit() or int(number)<0:
     number=input('eded musbet olmalidir.')
+divider = input('nece hisseye bolmek isteirsiniz?')
+while not divider.isdigit() or int(divider)<0 or len(number)%int(divider)!=0:
+    print('sehvlik bash verdi .nece hisseye bolmek isteirsiniz?')
+divider=int(divider)
+divided = len(number)//divider
+for i in range(divided):
+
+
